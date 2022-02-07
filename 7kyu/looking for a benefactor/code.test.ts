bf36a20cb6a00f8376d4930abf0def3f5aa12d70
jdelarubia <jdelarubia@gmail.com>
2022-02-07 22:46:25 +0100

    test looking for a benefactor

---
+++ 7kyu/looking for a benefactor/code.test.ts
@@ -0,0 +1,15 @@
+import { G964 } from "./code";
+
+function testing(arr: number[], newavg: number, expected: number) {
+  expect(G964.newAvg(arr, newavg)).toEqual(expected);
+}
+
+describe("Fixed Tests newAvg", function () {
+  it("Basic tests", function () {
+    testing([14, 30, 5, 7, 9, 11, 16], 90, 628);
+    testing([14, 30, 5, 7, 9, 11, 15], 92, 645);
+    expect(() => {
+      G964.newAvg([14, 30, 5, 7, 9, 11, 15], 2);
+    }).toThrow("Expected New Average is too low");
+  });
+});
