e92384c0d9289bf57a68379446ca8635aa9200fc
jdelarubia <jdelarubia@gmail.com>
2022-02-08 22:36:16 +0100

    test add a new base test to the suite

--- 7kyu/growth of a population/code.test.ts
+++ 7kyu/growth of a population/code.test.ts
@@ -1,7 +1,8 @@
 import { G964 } from "./code";
 
-describe("Fixed Tests", function () {
-  it("Basic tests nbYear", function () {
+describe("growth population fixed tests", function () {
+  it("Basic tests nbYear", () => {
+    expect(G964.nbYear(1000, 2, 50, 1200)).toEqual(3);
     expect(G964.nbYear(1500, 5, 100, 5000)).toEqual(15);
     expect(G964.nbYear(1500000, 2.5, 10000, 2000000)).toEqual(10);
     expect(G964.nbYear(1500000, 0.25, 1000, 2000000)).toEqual(94);
