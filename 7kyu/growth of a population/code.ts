afbc98a7352bfbe05d90cdf740ea93783b2e81de
jdelarubia <jdelarubia@gmail.com>
2022-02-08 22:35:42 +0100

    refactor clean synthetic test cases

--- 7kyu/growth of a population/code.ts
+++ 7kyu/growth of a population/code.ts
@@ -5,9 +5,8 @@ export class G964 {
     augment: number,
     targetPopulation: number
   ) => {
-    let currentPopulation =
-      (initPopulation * percent) / 100 + initPopulation + augment;
-    let years = 1;
+    let currentPopulation = initPopulation;
+    let years = 0;
 
     while (currentPopulation < targetPopulation) {
       currentPopulation += (currentPopulation * percent) / 100 + augment;
@@ -16,18 +15,3 @@ export class G964 {
     return years;
   };
 }
-
-const syntheticTests = [
-  {
-    actual: G964.nbYear(1000, 0.02, 50, 1200),
-    expected: 3,
-  },
-  { actual: G964.nbYear(1500000, 0.25, 10000, 2000000), expected: 94 },
-  { actual: G964.nbYear(1500000, 2.5, 10000, 2000000), expected: 10 },
-  { actual: G964.nbYear(1500, 5, 100, 5000), expected: 15 },
-];
-
-syntheticTests.forEach((test) => {
-  const { actual, expected } = test;
-  console.log(`actual: ${actual} | expected: ${expected}`);
-});
