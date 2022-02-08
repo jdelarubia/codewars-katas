e2b0f0404b33d91cc5c24a13cb6b9aaa61983177
jdelarubia <jdelarubia@gmail.com>
2022-02-08 22:33:07 +0100

    refactor cover corner case of an empty array

--- 7kyu/looking for a benefactor/code.ts
+++ 7kyu/looking for a benefactor/code.ts
@@ -1,8 +1,10 @@
 export class G964 {
   public static newAvg(arr: number[], newavg: number): any {
+    if (arr.length === 0) return newavg;
+
     const mean: number =
       arr.reduce((previous, current) => previous + current) / arr.length;
     if (newavg < mean) throw "Expected New Average is too low";
-    return (newavg - mean) * arr.length + newavg;
+    return Math.ceil((newavg - mean) * arr.length + newavg);
   }
 }
