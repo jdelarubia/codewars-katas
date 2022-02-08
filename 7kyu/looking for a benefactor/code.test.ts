32ed191e62d86b0e329605540ecd15f673b26b53
jdelarubia <jdelarubia@gmail.com>
2022-02-08 22:33:28 +0100

    test add a new test to check for empty array

--- 7kyu/looking for a benefactor/code.test.ts
+++ 7kyu/looking for a benefactor/code.test.ts
@@ -4,12 +4,18 @@ function testing(arr: number[], newavg: number, expected: number) {
   expect(G964.newAvg(arr, newavg)).toEqual(expected);
 }
 
-describe("Fixed Tests newAvg", function () {
+describe("Fixed Tests newAvg", () => {
   it("Basic tests", function () {
     testing([14, 30, 5, 7, 9, 11, 16], 90, 628);
     testing([14, 30, 5, 7, 9, 11, 15], 92, 645);
     expect(() => {
       G964.newAvg([14, 30, 5, 7, 9, 11, 15], 2);
     }).toThrow("Expected New Average is too low");
+  });
+});
+
+describe("Empty array", () => {
+  it("should return the value of the newavg parameter", () => {
+    testing([], 90, 90);
   });
 });
